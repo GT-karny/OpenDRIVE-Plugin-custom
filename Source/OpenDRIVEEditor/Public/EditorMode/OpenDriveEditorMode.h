aa381da6eaf2dbfcae44c11c2b3470c0b1b7c8c9
GT-karny <gakkusann3@gmail.com>
2026-02-04 09:36:34 +0900

    feat: 標識自動配置機能の実装

--- Source/OpenDRIVEEditor/Public/EditorMode/OpenDriveEditorMode.h
+++ Source/OpenDRIVEEditor/Public/EditorMode/OpenDriveEditorMode.h
@@ -152,6 +152,10 @@ public :
 	void SetGenerateSignals(bool Val) { bGenerateSignals = Val; }
 	bool GetGenerateSignals() const { return bGenerateSignals; }
 
+	bool bFlipSignalOrientation = false;
+	void SetFlipSignalOrientation(bool Val) { bFlipSignalOrientation = Val; }
+	bool GetFlipSignalOrientation() const { return bFlipSignalOrientation; }
+
 	USignalTypeMapping* SignalTypeMappingAsset = nullptr;
 	void SetSignalTypeMappingAsset(USignalTypeMapping* Asset) { SignalTypeMappingAsset = Asset; }
 	USignalTypeMapping* GetSignalTypeMappingAsset() const { return SignalTypeMappingAsset; }
